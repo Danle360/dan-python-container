b8a45b6bf244ea833ef0b4d0c461cb95fd796634
Danle360 <damandan360@gmail.com>
2024-12-31 22:34:09 -0600

    push

--- update_image_version.sh
+++ update_image_version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 imageVersion="danle360/python-dan-framwork:v$BUILD_NUMBER"
-sed -i "s#image_version_update#${imageVersion}#g" java_web_manifestFile.yml
-cat java_web_manifestFile.yml |grep  'danle'
+sed -i "s#image_version_update#${imageVersion}#g" framwork-deployment.yml
+cat framwork-deployment.yml |grep  'danle'
